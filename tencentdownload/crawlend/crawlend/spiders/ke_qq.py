a3112cf3a3f7f5a1150062324dd8e6a5e9e4a84e
HCF <binazicn@126.com>
2021-11-25 10:26:10 +0800

    2

--- tencentdownload/crawlend/crawlend/spiders/ke_qq.py
+++ tencentdownload/crawlend/crawlend/spiders/ke_qq.py
@@ -4,7 +4,7 @@ import scrapy
 class KeQqSpider(scrapy.Spider):
     name = 'ke_qq'
     allowed_domains = ['ke.qq.com']
-    start_urls = ['http://ke.qq.com/']
+    start_urls = ['http://ke.qq.com/',]
 
     def parse(self, response):
         pass
